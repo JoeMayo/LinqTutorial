02cf832ccb9db5cdcf765cb25b6370f3b9e200b3
Joe Mayo <jmayo@mayosoftware.com>
2020-08-09 22:51:04 -0700

    Added demo code for 01 - Intro to LINQ

--- 01-IntroToLinq/Program.cs
+++ 01-IntroToLinq/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01_IntroToLinq
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var gamers = new string[]
+            {
+                "Helen Jones",
+                "John Smith",
+                "Xavier Jones",
+                "Jane Doe"
+            };
+
+            var names =
+                from name in gamers
+                where name.EndsWith("Jones")
+                select name;
+
+            Console.WriteLine("Names ending in \"Jones\":\n");
+
+            foreach (var name in names)
+                Console.WriteLine("  - " + name);
+
+            //
+            // you can also use the fluent API
+            //
+
+            var namesFluent =
+                gamers
+                    .Where(name => name.EndsWith("Jones"))
+                    .Select(name => name);
+
+            Console.WriteLine("\n\nNames ending in \"Jones\":\n");
+
+            foreach (var name in namesFluent)
+                Console.WriteLine("  - " + name);
         }
     }
 }
